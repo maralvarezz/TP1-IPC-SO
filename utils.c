a972cee512da90694d83308582b0909096c4607b
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2025-03-19 12:58:56 -0300

    primeros archivos
    Co-Authored-By: Martin Gallardo Bertuzzi <146043951+Martin192003@users.noreply.github.com>
    Co-Authored-By: Maria Victoria Alvarez <146204185+maralvarezz@users.noreply.github.com>

---
+++ utils.c
@@ -0,0 +1,20 @@
+#include "./utils.h"
+
+void * createSHM(char * name, size_t size){
+    int fd = shm_open(name , O_RDWR |  O_CREAT, 0666);
+    if(fd ==-1){
+        perror("shm_open");
+        exit(EXIT_FAILURE);
+    }
+    if(-1 == ftruncate(fd, size)){
+        perror("ftruncate");
+        exit(EXIT_FAILURE);
+    }
+
+    void *toRet= mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    if(toRet == MAP_FAILED){
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+    return toRet;
+}
---
+++ utils.h
@@ -0,0 +1,20 @@
+
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <poll.h>
+#include <errno.h>
+#include <time.h>
+
+void * createSHM(char * name, size_t size);
+
