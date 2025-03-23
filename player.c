03392462f8e91095b22a2b2b29ce27397eb6efe3
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2025-03-23 18:01:35 -0300

    inteligencia basica
    algunas pruebas en player
    
    Co-Authored-By: Martin Gallardo Bertuzzi <146043951+Martin192003@users.noreply.github.com>
    Co-Authored-By: Maria Victoria Alvarez <146204185+maralvarezz@users.noreply.github.com>

--- player.c
+++ player.c
@@ -3,7 +3,8 @@
 #define CANTDIR 8
 
 void move(unsigned char * direction,sync_t *sems);
-int getMove(game_t * game, int playerNum);
+void getMove(game_t * game,int w,int h, int playerNum,sync_t *sems, unsigned char * direc);
+
 
 int main(int argc, char *argv[]){ // reciben w y h;
     if(argc != 3){ // siempre hay almenos un argumento que es el llamado al programa ./programa siempre es el primer argumento
@@ -36,21 +37,19 @@ int main(int argc, char *argv[]){ // reciben w y h;
 
     
     // Escribir la solicitud en el pipe
-    unsigned char dir;
     int aux = 15;
-    int seedAux = rand();
-    srand(playerNum*seedAux);
-
+    //int seedAux = rand();
+    //srand(playerNum*seedAux);
+    unsigned char dir;
     while(aux){// el master atiendo preguntando si hay algun moviemiento en orden, si no hay va a al siguiente
         //if (playerNum==4){
            // sleep(4);
         //}
-        dir = rand()%8;
-        //dir = getMove(game,playerNum);
-        //if(dir == -1){
+        getMove(game,w,h,playerNum, sems, &dir);
+        if(dir == 15){
         //   game->players[playerNum].blocked = true;
-         //   break;
-       // }
+            break;
+        } 
         move(&dir,sems);
         aux--;
     }
@@ -63,39 +62,59 @@ int main(int argc, char *argv[]){ // reciben w y h;
 //int pipe(int pipefd[2]);
 //int pipe2(int pipefd[2], int flags);
 void move(unsigned char * direction,sync_t *sems){
-    sem_wait(&sems->C);
+    //sem_wait(&sems->C);
     // Escribir la solicitud en el pipe
     if (write(WRITEFD, direction, sizeof(unsigned char)) == -1) {
         perror("write");
-        sem_post(&sems->C); 
+       //sem_post(&sems->C); 
         return;
     }
-    sem_post(&sems->C);
+    //sem_post(&sems->C);
     return;
 }
 
 /**
-    * Devuelve la direccion de movimiento y -1 si no hay direccion disponible.
+    * Devuelve la direccion de movimiento y 15 si no hay direccion disponible.
  */
 
-int getMove(game_t * game, int playerNum){
+ void getMove(game_t * game,int w,int h, int playerNum,sync_t *sems,unsigned char * direct){
+    int dirs[][2]= {{0,-1},
+                    {1,-1},
+                    {1,0},
+                    {1,1},
+                    {0,1},
+                    {-1,1},
+                    {-1,0},
+                    {-1,-1}};
     int max = 0;
-    int dir = -1;
+    *direct = 15; 
+    
+    sem_wait(&sems->E);
+    if(++sems->playersReading==1){
+        sem_wait(&sems->D);
+    }
+    sem_post(&sems->E);
+    
     unsigned short y = game->players[playerNum].posY;
     unsigned short x = game->players[playerNum].posX;
-    
-    int w = game->width;
     //Sacamos cual es el adyacente mayor
-    for(int a1 = -1; a1 <= 1; a1++){
-        for(int a2 = -1; a2 <= 1; a2++){
-            if(game->board[y*(w+a1)+x+a2] > max){
-                max = game->board[y*(w+a1)+x+a2];
-                dir = (y*(w+a1)+x+a2) % 9;
+    int aux;
+    for(int i = 0; i < 8; i++){
+        if(y+dirs[i][1]>=0 && y+dirs[i][1]<h && dirs[i][0]+x >=0 && dirs[i][0]+x < w ){
+            if((aux = game->board[w*(y+dirs[i][1])+dirs[i][0]+x] ) > max){ 
+                max = aux;
+                *direct = i;
                 if(max == 9){
-                    return dir;
+                    break;
                 }
             }
-        }
+        }    
     }
-    return dir;
+    
+    sem_wait(&sems->E);
+    if(--sems->playersReading==0){
+        sem_post(&sems->D);
+    }
+    sem_post(&sems->E);
+
 }
